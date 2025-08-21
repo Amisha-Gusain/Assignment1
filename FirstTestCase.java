6aa0e5a10560dd6ea607e40e2bf5b5bf751ea90c
Amisha-Gusain <Amisha.Gusain@geminisolutions.com>
2025-08-21 15:35:24 +0530

    Add files via upload

---
+++ FirstTestCase.java
@@ -0,0 +1,21 @@
+package com.example.testngmain;
+
+import org.testng.annotations.Test;
+
+public class FirstTestCase {
+
+    @Test
+    public void testOne() {
+        System.out.println("FirstTestCase → testOne");
+    }
+
+    @Test
+    public void testTwo() {
+        System.out.println("FirstTestCase → testTwo");
+    }
+
+    @Test
+    public void testThree() {
+        System.out.println("FirstTestCase → testThree");
+    }
+}
---
+++ SecondTestCase.java
@@ -0,0 +1,22 @@
+package com.example.testngmain;
+
+import org.testng.annotations.Test;
+
+public class SecondTestCase {
+
+    @Test
+    public void testA() {
+        System.out.println("SecondTestCase → testA");
+    }
+
+    @Test
+    public void testB() {
+        System.out.println("SecondTestCase → testB");
+    }
+
+    @Test
+    public void testC() {
+        System.out.println("SecondTestCase → testC");
+    }
+}
+
